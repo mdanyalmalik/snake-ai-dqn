b1de5a02deff38efc974e52c0740fb3f1a582de7
Danyal <danyalm2002@gmail.com>
2022-07-22 11:47:32 +0500

    added comments to constants file

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,10 @@
+# colours (rgb)
 BACKGROUND = (14, 20, 26)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+# values in pixels
 SIZE = WIDTH, HEIGHT = 800, 600
 BLOCKS_X = 40
 GRID_DIV = WIDTH//BLOCKS_X
