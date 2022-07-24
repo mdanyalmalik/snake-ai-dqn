2e0666a7134c1175ff61a8a1fedcc76aed345c0c
Danyal <danyalm2002@gmail.com>
2022-07-24 21:40:48 +0500

    added comments to main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from constants import *
 from main_menu import main_menu
 
 if __name__ == '__main__':
-    pg.init()
+    pg.init()  # initialise pygame for the whole program
 
     win = pg.display.set_mode(SIZE)
     pg.display.set_caption('Snake')
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     icon = pg.image.load('../res/snake.png')
     pg.display.set_icon(icon)
 
-    main_menu(win)
+    main_menu(win)  # launch main menu, the rest of the logic handled there
